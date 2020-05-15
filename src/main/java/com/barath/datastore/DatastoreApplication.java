4746e4d890bd07d7da3c0488dfabf0b9baef596c
Barath Karunakaran <barath.karunakaran@cowrks.com>
2020-05-15 15:11:14 +0530

    initial commit

---
+++ src/main/java/com/barath/datastore/DatastoreApplication.java
@@ -0,0 +1,17 @@
+package com.barath.datastore;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+@Slf4j
+public class DatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/barath/datastore/config/WebConfiguration.java
@@ -0,0 +1,28 @@
+package com.barath.datastore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/*
+ * Created by Barath.
+ */
+@Configuration
+public class WebConfiguration {
+    /**
+     * Method for MVC Confug
+     *
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
